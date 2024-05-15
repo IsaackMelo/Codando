8bb2f8649338e3cc024413a55a8f0f384a34af3c
Isaac Kauan Silva de Melo <isaackaua23@gmail.com>
2024-05-15 14:58:28 -0300

    init commit

---
+++ f.unção.js
@@ -0,0 +1,16 @@
+function verify(number){
+console.log("Existem 3 tipos de cabelo");
+console.log("1 = Muito cabelo");
+console.log("2 = Entradas marcantes");
+console.log("3 = Sem cabelo.");
+}
+const number = 2
+if (number == 2) {
+    console.log("Você não é calvo, com certeza não é o Igor");
+}
+else if (number < 2) {
+    console.log("Você é cabeludo, mas se for o Igor você se enganou");
+}
+else if (number > 2); {
+    console.log("Igor seu careca")
+}
---
+++ swith.js
@@ -0,0 +1,11 @@
+const nome = "Paulo";
+switch (nome) {
+    case "Igor": {
+        console.log("Você não me escapa seu careca");
+        break;
+    }
+    default: {
+        console.log("Você não é o igor, você não é careca :)");
+        break;
+    }
+}
